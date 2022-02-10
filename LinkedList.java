b29643621c0d967095ceb7b1bc3e55de85989848
Sujit <sujit.mollet@rutgers.edu>
2022-02-09 19:49:00 -0500

    commit

---
+++ LinkedList.java
@@ -0,0 +1,63 @@
+class LinkedList{
+
+	LLNode head;
+
+	private class LLNode{
+		int value;
+		LLNode next;
+
+		public LLNode(int value, LLNode next){
+			this.value = value;
+			this.next = next;
+		}
+	}
+		
+	public void insertNode(int value){
+		LLNode tmp = new LLNode(value, null);
+
+		if(head == null){
+			head = tmp;
+		}
+
+		LLNode ptr = head;
+
+		while(ptr.next != null){
+			ptr = ptr.next;
+		}
+		ptr.next = tmp;
+	}
+
+	public void printList(){
+
+		LLNode ptr = head;
+
+		while(ptr != null){
+			System.out.println(ptr.value + "->");
+		}
+	}
+
+	public LLNode deleteNode(int value){
+
+		return null;
+	}
+
+	public LLNode removeAfter(int value){
+
+		// Write code here
+
+		return null;
+	}
+
+	public LLNode getMax(){
+
+		// Write code here
+				
+		return null;
+	}
+
+	public LLNode partition(){
+
+		return null;
+	}
+
+}
---
+++ Makefile
@@ -0,0 +1,9 @@
+three: 
+	javac MyStack.java
+	javac LinkedList.java
+	javac Driver.java
+
+clean:
+	rm MyStack*.class
+	rm Driver.class
+	rm LinkedList*.class
